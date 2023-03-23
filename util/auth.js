ce42ae8fea8b21df72583af8713cdfca56c7d4f7
Uthaeus <romanlavery@gmail.com>
2023-03-23 09:37:41 -0600

    created login helper function, added logic to LoginScreen

--- util/auth.js
+++ util/auth.js
@@ -1,13 +1,24 @@
 import axios from "axios";
 
+// example using firebase authentication api
 const API_KEY = "your-api-key";
 
-export async function createUser(email, password) {
-  const response = await axios.post("api-auth-address" + API_KEY, {
+async function authenticate(mode, email, password) {
+  const url = `aip-auth-address${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
     email: email,
     password: password,
     returnSecureToken: true,
   });
 
 
+}
+
+export async function createUser(email, password) {
+  await authenticate('signUp', email, password);
+}
+
+export async function login(email, password) {
+    await authenticate('signInWithPassword', email, password);
 }
