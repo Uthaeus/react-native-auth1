138238361d7bc712cc36b62779410f4a8426a625
Uthaeus <romanlavery@gmail.com>
2023-03-23 11:30:37 -0600

    created root component to check asyncstorage for existing authentication token, added method in logout to remove token from asyncstorage

--- store/auth-context.js
+++ store/auth-context.js
@@ -1,31 +1,34 @@
 import { createContext, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const AuthContext = createContext({
-    token: '',
-    isAuthenticated: false,
-    authenticate: (token) => {},
-    logout: () => {}
+  token: "",
+  isAuthenticated: false,
+  authenticate: (token) => {},
+  logout: () => {},
 });
 
-function AuthContextProvider({children}) {
-    const [authToken, setAuthToken] = useState();
+function AuthContextProvider({ children }) {
+  const [authToken, setAuthToken] = useState();
 
-    function authenticate(token) {
-        setAuthToken(token);
-    }
+  function authenticate(token) {
+    setAuthToken(token);
+    AsyncStorage.setItem("token", token);
+  }
 
-    function logout() {
-        setAuthToken(null);
-    }
+  function logout() {
+    setAuthToken(null);
+    AsyncStorage.removeItem('token');
+  }
 
-    const value = {
-        token: authToken,
-        isAuthenticated: !!authToken,
-        authenticate: authenticate,
-        logout: logout
-    };
+  const value = {
+    token: authToken,
+    isAuthenticated: !!authToken,
+    authenticate: authenticate,
+    logout: logout,
+  };
 
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
 export default AuthContextProvider;
